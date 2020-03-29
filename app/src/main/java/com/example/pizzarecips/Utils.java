f0599e1c1af26d55aae3816f4edc04047a49a61a
Kholkin <kholkins@mail.ru>
2020-03-29 19:42:20 +0300

    Add Pizza item list

---
+++ app/src/main/java/com/example/pizzarecips/Utils.java
@@ -0,0 +1,17 @@
+package com.example.pizzarecips;
+
+public class Utils {
+    public static final String TITLE_1 = "Pizza";
+    public static final String DESCRIPTION_1 = "Make your own pizza with a crispy base, mozzarella cheese and a fresh tomato sauce. Add simple toppings like slices of ham and rocket if you like.";
+    public static final String RECIPE_1 = "Mix together the yeast and sugar with 250ml warm water and leave to sit for 10 mins. Place half the flour in a table-top mixer with a dough hook, pour in the yeast mixture and beat at medium speed for 10 mins (or mix in a bowl, then knead with oiled hands in the bowl for 5-10 mins).\n" +
+            "\n" +
+            "Leave somewhere warm for 10 more mins, then add the remaining flour and oil. Beat or knead to a dough for a further 5 mins. Put in a well-oiled bowl, cover with a cloth and place somewhere warm to double in size – about 1½ hrs.\n" +
+            "\n" +
+            "For the sauce, heat the oil in a pan over a moderate heat. Add the onion and cook for 3 mins, stirring constantly. Add the garlic, 1 tsp sea salt and 1 tsp ground black pepper and cook for 2 mins more. Add the tomatoes and bring to the boil, then reduce the heat and simmer for a good 20 mins, stirring occasionally.\n" +
+            "\n" +
+            "Once your dough has doubled in size, slap it down on a lightly floured surface and knead for 4 mins until soft but not too elastic. Divide into 6 pieces, roll into balls and leave to rest for 10 mins. Heat oven to the highest temperature possible (270C fan in the Good Food test kitchen) and place a flat baking sheet in the oven.\n" +
+            "\n" +
+            "With a well-floured rolling pin, roll out each ball of dough as thinly as possible. Remove the baking sheet from the oven, oil or dust with flour, then carefully transfer the dough base onto it. Spread over some of the tomato sauce (I like to apply it in the quantity that you would spread Marmite on toast, or the base will be soft and fall to bits), then the cheese and any toppings you desire.\n" +
+            "\n" +
+            "Cook for between 5-10 mins depending on your oven temp, until the base is crisp and the cheese melted. Scatter with basil.";
+}
