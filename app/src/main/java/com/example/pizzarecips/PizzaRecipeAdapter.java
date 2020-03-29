300f9187e4c5934710ea6e7234a9d67ca773f064
Kholkin <kholkins@mail.ru>
2020-03-29 19:01:32 +0300

    Add Pizza item adapter

--- app/src/main/java/com/example/pizzarecips/PizzaRecipeAdapter.java
+++ app/src/main/java/com/example/pizzarecips/PizzaRecipeAdapter.java
@@ -1,6 +1,39 @@
 package com.example.pizzarecips;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class PizzaRecipeAdapter extends RecyclerView.Adapter {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class PizzaRecipeViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView imageView;
+        public TextView title;
+        public TextView description;
+
+        public PizzaRecipeViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
