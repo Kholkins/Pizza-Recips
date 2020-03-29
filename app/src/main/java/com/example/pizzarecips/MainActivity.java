415a13945a8b2fb9ee01d42a08aa4df88b8a9398
Kholkin <kholkins@mail.ru>
2020-03-29 20:51:39 +0300

    Add Pizza item list

--- app/src/main/java/com/example/pizzarecips/MainActivity.java
+++ app/src/main/java/com/example/pizzarecips/MainActivity.java
@@ -24,5 +24,51 @@ public class MainActivity extends AppCompatActivity {
                 Utils.DESCRIPTION_1,
                 Utils.RECIPE_1
                 ));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza2,
+                Utils.TITLE_2,
+                Utils.DESCRIPTION_2,
+                Utils.RECIPE_2
+                ));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza3,
+                Utils.TITLE_3,
+                Utils.DESCRIPTION_3,
+                Utils.RECIPE_3
+                ));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza4,
+                Utils.TITLE_4,
+                Utils.DESCRIPTION_4,
+                Utils.RECIPE_4
+                ));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza_5,
+                Utils.TITLE_5,
+                Utils.DESCRIPTION_5,
+                Utils.RECIPE_5
+                ));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza6,
+                Utils.TITLE_6,
+                Utils.DESCRIPTION_6,
+                Utils.RECIPE_6
+                ));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.piza7,
+                Utils.TITLE_7,
+                Utils.DESCRIPTION_7,
+                Utils.RECIPE_7
+                ));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza8,
+                Utils.TITLE_8,
+                Utils.DESCRIPTION_8,
+                Utils.RECIPE_8
+                ));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza9,
+                Utils.TITLE_9,
+                Utils.DESCRIPTION_9,
+                Utils.RECIPE_9
+                ));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.pizza10,
+                Utils.TITLE_10,
+                Utils.DESCRIPTION_10,
+                Utils.RECIPE_10
+                ));
+
     }
 }
--- app/src/main/java/com/example/pizzarecips/Utils.java
+++ app/src/main/java/com/example/pizzarecips/Utils.java
@@ -14,4 +14,60 @@ public class Utils {
             "With a well-floured rolling pin, roll out each ball of dough as thinly as possible. Remove the baking sheet from the oven, oil or dust with flour, then carefully transfer the dough base onto it. Spread over some of the tomato sauce (I like to apply it in the quantity that you would spread Marmite on toast, or the base will be soft and fall to bits), then the cheese and any toppings you desire.\n" +
             "\n" +
             "Cook for between 5-10 mins depending on your oven temp, until the base is crisp and the cheese melted. Scatter with basil.";
+    public static final String TITLE_2 = "Pizza Margherita in 4 easy steps";
+    public static final String DESCRIPTION_2 = "Even a novice cook can master the art of pizza with our simple step-by-step guide. Bellissimo";
+    public static final String RECIPE_2 = "Make the base: Put the flour into a large bowl, then stir in the yeast and salt. Make a well, pour in 200ml warm water and the olive oil and bring together with a wooden spoon until you have a soft, fairly wet dough. Turn onto a lightly floured surface and knead for 5 mins until smooth. Cover with a tea towel and set aside. You can leave the dough to rise if you like, but it’s not essential for a thin crust.\n" +
+            "\n" +
+            "Make the sauce: Mix the passata, basil and crushed garlic together, then season to taste. Leave to stand at room temperature while you get on with shaping the base.\n" +
+            "\n" +
+            "Roll out the dough: If you’ve let the dough rise, give it a quick knead, then split into two balls. On a floured surface, roll out the dough into large rounds, about 25cm across, using a rolling pin. The dough needs to be very thin as it will rise in the oven. Lift the rounds onto two floured baking sheets.\n" +
+            "\n" +
+            "Top and bake: Heat oven to 240C/fan 220C /gas 8. Put another baking sheet or an upturned baking tray in the oven on the top shelf. Smooth sauce over bases with the back of a spoon. Scatter with cheese and tomatoes, drizzle with olive oil and season. Put one pizza, still on its baking sheet, on top of the preheated sheet or tray. Bake for 8-10 mins until crisp. Serve with a little more olive oil, and basil leaves if using. Repeat step for remaining pizza.";
+    public static final String TITLE_3 = "Superhealthy pizza";
+    public static final String DESCRIPTION_3 = "The quantities for this are generous, so if you have any leftovers, pop a wedge of cold pizza into your lunchbox the next day";
+    public static final String RECIPE_3 = "Mix the flours and yeast with a pinch of salt in a food processor fitted with a dough blade. Pour in the water and mix to a soft dough, then work for 1 min. Remove the dough and roll out on a lightly floured surface to a round about 30cm across. Lift onto an oiled baking sheet.\n" +
+            "\n" +
+            "Spread the canned tomatoes over the dough to within 2cm of the edges. Arrange the cherry tomatoes and courgettes over the top, then scatter with the mozzarella. Mix the capers, olives and garlic, then scatter over the top. Drizzle evenly with the oil. Leave to rise for 20 mins. Heat oven to 240C/ fan 220C/gas 9 or the highest setting.\n" +
+            "\n" +
+            "Bake the pizza for 10-12 mins until crisp and golden around the edges. Scatter with the parsley to serve.";
+    public static final String TITLE_4 = "Florentine pizza";
+    public static final String DESCRIPTION_4 = "An easy vegetarian supper or a satisfying weekend lunch";
+    public static final String RECIPE_4 = "To prepare the pizza, preheat the oven to 200C/Gas 6/fan 180C. Put the pizza base on a baking sheet and spread with the sauce.\n" +
+            "\n" +
+            "To cook the spinach, put it in a microwavable bowl, cover with cling film and pierce it a couple of times. Put in the microwave on High (850W) for 2 minutes until wilted (or cook in a covered pan for 2-3 minutes). Drain and spread the spinach over the pizza base, then get the mushrooms out of the jar with a fork and scatter them over the top. Season and sprinkle with half the parmesan. Bake for 10 minutes.\n" +
+            "\n" +
+            "Make four dips in the spinach with the back of a spoon, and crack an egg into each. Sprinkle with the remaining cheese and bake for a further 6- 8 minutes, until the eggs are just set.";
+    public static final String TITLE_5 = "Pizza omelette";
+    public static final String DESCRIPTION_5 = "This unusual combination is a real winner and is a great dish to cook with the kids";
+    public static final String RECIPE_5 = "Heat grill to high. Beat the eggs with the oregano and some salt and pepper. Heat the oil in a large ovenproof frying pan, then tip in the eggs. Cook over a low-ish heat for 4 mins, stirring occasionally until the eggs are practically set.\n" +
+            "\n" +
+            "Place under the grill until set and puffed up. Spread the sauce over, scatter with cheese and olives, then grill again until cheese is melted. Cut into wedges and serve with a green salad.";
+    public static final String TITLE_6 = "Pizza pie";
+    public static final String DESCRIPTION_6 = "Use a ready-made bread mix to make this homemade calzone-style pizza filled with cheese, tomato, pepperoni and peppers";
+    public static final String RECIPE_6 = "Heat oven to 220C/200C fan/gas 7. Make up the pizza base following pack instructions. Knead on a lightly floured surface and divide into 2 balls, 1 slightly bigger than the other. Roll out the bigger ball to about 32cm diameter. Place on a floured baking sheet or pizza tray.\n" +
+            "\n" +
+            "Top the pizza base with the pasta sauce, pepperoni, onion, pepper, mozzarella, oregano, 1 tbsp of the Parmesan and a little black pepper.\n" +
+            "\n" +
+            "Roll the smaller piece of dough to about 28cm diameter and place on top of the pizza filling. Fold the edge of the base over the top, pressing the edges together to seal – use a little water to help stick if you need to. Sprinkle over the remaining Parmesan and bake for 15 mins until puffed and golden.";
+    public static final String TITLE_7 = "Rainbow pizzas";
+    public static final String DESCRIPTION_7 = "Forget takeaway pizzas, encourage kids to eat a rainbow every day with this colourful recipe full of fresh flavours – guaranteed to please even fussy eaters";
+    public static final String RECIPE_7 = "Heat the oven to 180C/160C fan/gas 4. Put each pizza base on a baking sheet and spread each with half of the passata. Arrange the tomatoes on the top in rings or wedges of colour and add the broccoli and the olives, if using. Squish the mozzarella cherries (bocconcini) a little before dotting them over the pizzas, then drizzle 1 tbsp pesto over each.\n" +
+            "\n" +
+            "Bake for 15-20 mins or until the top is bubbling and just starting to brown a little. Scatter over the basil leaves before serving.";
+    public static final String TITLE_8 = "Flatbread pizzas";
+    public static final String DESCRIPTION_8 = "Whizz up your own homemade hummus and spread onto flatbreads then add toppings for a healthy pizza";
+    public static final String RECIPE_8 = "Blitz the chickpeas, half the lemon juice, garlic, cumin and some seasoning in a food processor or mini chopper, adding a few tbsp water to get a spreadable consistency.\n" +
+            "\n" +
+            "Warm the flatbreads following pack instructions, then spread the chickpea hummus over them evenly. Scatter on the feta cheese, dill and half the rocket, pomegranate and onion.\n" +
+            "\n" +
+            "Mix together the remaining rocket, pomegranate and onion. Toss through the remaining lemon juice, olive oil and any juice from the pomegranate seeds. Serve the pizzas with the salad.";
+    public static final String TITLE_9 = "Christmas pizza";
+    public static final String DESCRIPTION_9 = "Use up leftover roast turkey and sausagemeat stuffing in this new spin on an Italian classic";
+    public static final String RECIPE_9 = "Heat oven to 220C/200C fan/gas 7. Prepare the pizza base mix following pack instructions. Once rolled out, leave to rest for 10 mins, then top with the pasta sauce.\n" +
+            "\n" +
+            "Scatter over the stuffing and turkey, then top with the mozzarella. Toss the sage leaves with the oil, then scatter over the pizza, drizzling over any remaining oil. Bake for 10-12 mins until the crust is crisp and the cheese has melted.";
+    public static final String TITLE_10 = "Watermelon pizza";
+    public static final String DESCRIPTION_10 = "Make this fun fruit 'pizza' as a side dish, or a light dessert at a picnic or barbecue. The watermelon, pineapple and coconut really sing summer.";
+    public static final String RECIPE_10 = "Take 1 round slice watermelon and cut into wedges, as you would a pizza, and lay them on a board. Scatter over 120g pineapple pieces, a few coriander or Thai basil leaves and 1-2 tsp toasted coconut flakes. Mix the juice ½ lime with 1 tbsp honey and drizzle over the top. Sprinkle with freshly cracked black pepper and zest ½ lime.";
+
 }
